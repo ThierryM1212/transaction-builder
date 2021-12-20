c44a74617feb3d4271108b63b7eacfabfb560f65
thierryM1212 <thierry.mangiante@gmail.com>
2021-12-20 02:27:30 +0100

    fix token list when empty

--- src/components/UtxosSummary.js
+++ src/components/UtxosSummary.js
@@ -1,28 +1,30 @@
 import { getUtxosListValue, getTokenListFromUtxos } from '../ergo-related/utxos';
 
 export default function UtxosSummary(props) {
+
+    const ergAmount = parseFloat(parseInt(getUtxosListValue(props.list)) / 1000000000).toFixed(4);
+    const tokenDict = getTokenListFromUtxos(props.list);
+    console.log("UtxosSummary",Object.keys(tokenDict).length);
     return (
         <div className="card m-1 p-1" >
             <div className="d-flex flex-row">
-            <h6>Total selected:&nbsp; {parseFloat(parseInt(getUtxosListValue(props.list)) / 1000000000).toFixed(4)} ERG</h6>
+            <h6>Total selected:&nbsp; {ergAmount} ERG</h6>
             </div>
-            { getTokenListFromUtxos(props.list) === {} ? 
+            { Object.keys(tokenDict).length > 0 ?
             <table border="1" >
                 <thead>
                     <tr><td><h6>Tokens</h6></td>
                         <td><h6>Amount</h6></td></tr>
                 </thead>
-                
                 <tbody>
                     {
-                        Object.entries(getTokenListFromUtxos(props.list)).map(([key, value]) => (
+                        Object.entries(tokenDict).map(([key, value]) => (
                             <tr key={key}><td>{key}</td><td>{value}</td></tr>
                         ))
                     }
                 </tbody>
-
             </table>
-            : <h6>No token selected</h6>
+            : <h6>No token in selected input boxes</h6>
             }
             
         </div>
