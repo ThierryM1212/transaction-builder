a7b5d05ccd29a3036928b5a1c77a67eb46e5933a
thierryM1212 <thierry.mangiante@gmail.com>
2021-12-22 11:29:28 +0100

    proxy used ergo node features to the backend node

--- src/ergo-related/node.js
+++ src/ergo-related/node.js
@@ -1,6 +1,10 @@
 import { get } from './rest';
 
-export const nodeApi = 'http://149.202.46.53:9053';
+// in order to secure the node requests (port 9053) the following setting have been done on apache
+// prevent any connection to 9053 except from localhost
+// proxy https://transaction-builder.ergo.ga/blocks to http://localhost:9053/blocks/lastHeaders/10
+
+export const nodeApi = 'https://transaction-builder.ergo.ga';
 
 async function getRequest(url) {
     return get(nodeApi + url).then(res => {
@@ -9,6 +13,6 @@ async function getRequest(url) {
 }
 
 export async function getLastHeaders() {
-    return getRequest('/blocks/lastHeaders/10')
+    return getRequest('/blocks/')
         .then(res => res.data)
 }
